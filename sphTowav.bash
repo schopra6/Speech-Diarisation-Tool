c5950026821484bc28559196d715888400d097f1
schopra6 <shaneamgarcia@gmail.com>
2021-11-24 09:34:46 -0500

    sphTowav implemented

---
+++ sphTowav.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Make a copy of the data
+#goes through all folders in current directory (*)
+#and store data in directory.txt
+find * -type f > directory.txt
+#loop through the all files and make .wav file
+while read file; do
+  echo "$file converted"
+  echo "new file name ${file%.sph}.wav"
+  newfile=${file%.sph}.wav
+  echo "$newfile"
+  ffmpeg -i $file $newfile
+done <directory.txt
